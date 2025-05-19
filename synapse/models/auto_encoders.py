3d67ea59af79fd0c87d4af96dbe70f56cb413d65
Pedro Fluxa <pfluxa@Pedros-MacBook-Air.local>
2025-05-19 08:59:14 -0400

    better mixture

--- synapse/models/auto_encoders.py
+++ synapse/models/auto_encoders.py
@@ -119,7 +119,7 @@ class TabularBERT(nn.Module):
         rec_loss = torch.mean((decoded - cmb_emb)**2)
 
         # spherical loss
-        w_sph = self.smooth_growth(epoch, 1, 100, low_val=1e-5, high_val=1e-1)
+        w_sph = self.smooth_growth(epoch, 0, 10, low_val=1e-5, high_val=1.0)
         sph_loss, sph_metrics = self.sph_loss_fn(codecs)
 
         total_loss = rec_loss + w_sph * sph_loss
